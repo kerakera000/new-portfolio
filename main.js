c41fc73bcefbcd39a1129f0a1386911d600d33e7
LEon <leon51414@icloud.com>
2021-07-02 20:21:28 +0900

    headerまで完成

---
+++ main.js
@@ -0,0 +1,9 @@
+const header1 = document.getElementById('H-IMG1');
+const header2 = document.getElementById('H-IMG2');
+const header3 = document.getElementById('H-IMG3');
+
+window.onload = function(){
+    header1.classList.add('H-IMG-toggle1');
+    header2.classList.add('H-IMG-toggle2');
+    header3.classList.add('H-IMG-toggle3');
+}
