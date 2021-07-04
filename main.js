0b3906bfee97d6b31a84b04c2c6b53a5da27b12a
LEon <leon51414@icloud.com>
2021-07-05 01:23:14 +0900

    header js完成

--- main.js
+++ main.js
@@ -1,22 +1,33 @@
 //ロード時にクラス追加
-const header1 = document.getElementById('H-IMG1');
-const header2 = document.getElementById('H-IMG2');
-const header3 = document.getElementById('H-IMG3');
+const navlink0 = document.getElementById('nav0');
+const navlink1 = document.getElementById('nav1');
+const navlink2 = document.getElementById('nav2');
+const navlink3 = document.getElementById('nav3');
 
-const navlink0 = doqument.getElementById('nav0');
-const navlink1 = doqument.getElementById('nav1');
-const navlink2 = doqument.getElementById('nav2');
-const navlink3 = doqument.getElementById('nav3');
+$(window).on('load', function (){
+  setTimeout(function(){
+		navlink0.classList.add('nav-link');
+    navlink1.classList.add('nav-link');
+    navlink2.classList.add('nav-link');
+    navlink3.classList.add('nav-link');
+	},1600);
+});
 
-window.onload = function(){
-    header1.classList.add('H-IMG-toggle1');
-    header2.classList.add('H-IMG-toggle2');
-    header3.classList.add('H-IMG-toggle3');
-    navlink0.classList.add('nav-link0');
-    navlink1.classList.add('nav-link1');
-    navlink2.classList.add('nav-link2');
-    navlink3.classList.add('nav-link3');
-}
+//トップに戻るたびにimgにクラスを付与
+$(window).on('load scroll', function() {
+  if($(window).scrollTop() > 700) {
+    $('.title').removeClass('visible');
+    $('#H-IMG1').removeClass('H-IMG-toggle1');
+    $('#H-IMG2').removeClass('H-IMG-toggle2');
+    $('#H-IMG3').removeClass('H-IMG-toggle3');
+  } else {
+    $('.title').addClass('visible');
+    $('#H-IMG1').addClass('H-IMG-toggle1');
+    $('#H-IMG2').addClass('H-IMG-toggle2');
+    $('#H-IMG3').addClass('H-IMG-toggle3');
+  }
+});
+
 //リンククリック時スクロール
 $(function(){
     $('a[href^="#"]').click(function(){
